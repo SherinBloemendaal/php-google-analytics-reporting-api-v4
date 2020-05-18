f2502ecbf6ed5a926fca7b680b96152bb35efd40
Sherin <sherin@blueberry.nl>
2020-05-18 16:14:34 +0200

    1.0.19 2020-05-18 Bugfix release, Update not required - Bugfix: remove php cs fixer cache + composer.lock - Feature: Add new template PopularPageByDimension that retrieves popular pages based on a custom dimension. - Feature: Add CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+##### 1.0.19 2020-05-18 Bugfix release, Update not required
+- Bugfix: remove php cs fixer cache + composer.lock
+- Feature: Add new template PopularPageByDimension that retrieves popular pages based on a custom dimension.
+- Feature: Add CHANGELOG
---
+++ src/Template/PopularPageByDimension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace sherin\google\analytics\Template;
+
+use sherin\google\analytics\Dimension\Dimension;
+use sherin\google\analytics\Metric\Metric;
+use sherin\google\analytics\Order\Order;
+use sherin\google\analytics\Order\OrderBy;
+use sherin\google\analytics\Query\Direction;
+use sherin\google\analytics\Query\QueryBuilder;
+
+class PopularPageByDimension
+{
+    public static function getQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
+    {
+        $queryBuilder = new QueryBuilder($queryBuilder);
+        $queryBuilder
+            ->addDimension(new Dimension("ga:dimension3"))
+            ->addMetric(new Metric("ga:uniquePageViews"))
+            ->addMetric(new Metric("ga:bounceRate"))
+            ->addMetric(new Metric("ga:pageviews"))
+            ->addMetric(new Metric("ga:avgTimeOnPage"))
+            ->addMetric(new Metric("ga:entrances"))
+            ->addMetric(new Metric("ga:exits"))
+            ->addMetric(new Metric("ga:uniqueEvents"))
+            ->addOrderBy(new Order("ga:pageviews", OrderBy::VALUE, Direction::DESCENDING))
+            ->setMaxResults(10);
+        ;
+        return $queryBuilder;
+    }
+}
